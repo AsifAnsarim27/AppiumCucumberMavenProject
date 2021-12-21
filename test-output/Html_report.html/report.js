f62878015d7943d5f11c7c0a448f125b09f9444e
AsifAnsarim27 <96493069+AsifAnsarim27@users.noreply.github.com>
2021-12-22 02:30:13 +0530

    Add files via upload

---
+++ test-output/Html_report.html/report.js
@@ -0,0 +1,770 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("DisplayTitleOption.feature");
+formatter.feature({
+  "line": 2,
+  "name": "To verify the click functionality of Display_Show_Title option in App section of  DemoApi app.",
+  "description": "",
+  "id": "to-verify-the-click-functionality-of-display-show-title-option-in-app-section-of--demoapi-app.",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 13233828400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 6,
+  "name": "Display_show_title Check",
+  "description": "",
+  "id": "to-verify-the-click-functionality-of-display-show-title-option-in-app-section-of--demoapi-app.;display-show-title-check",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@test3"
+    }
+  ]
+});
+formatter.step({
+  "line": 8,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "user tap on the App section of app",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "tap on the Action Bar option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "tap on the Display Option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "as click on Display Show Title button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "path of that button starts toggling on the top",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 801889200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DisplayTitleSteps.user_tap_on_the_App_section_of_app()"
+});
+formatter.result({
+  "duration": 1805452200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DisplayTitleSteps.tap_on_the_Action_Bar_option()"
+});
+formatter.result({
+  "duration": 196562900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DisplayTitleSteps.tap_on_the_Display_Option()"
+});
+formatter.result({
+  "duration": 2895253800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DisplayTitleSteps.as_click_on_Display_Show_Title_button()"
+});
+formatter.result({
+  "duration": 758633400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DisplayTitleSteps.path_of_that_button_get_disappeared_from_the_top()"
+});
+formatter.result({
+  "duration": 941300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 302665600,
+  "status": "passed"
+});
+formatter.uri("FliptheList.feature");
+formatter.feature({
+  "line": 1,
+  "name": "To verify and check the action of flip button under animation section of the  app.",
+  "description": "",
+  "id": "to-verify-and-check-the-action-of-flip-button-under-animation-section-of-the--app.",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 12082757900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 6,
+  "name": "Check Flip Action of whole list",
+  "description": "",
+  "id": "to-verify-and-check-the-action-of-flip-button-under-animation-section-of-the--app.;check-flip-action-of-whole-list",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@test6"
+    }
+  ]
+});
+formatter.step({
+  "line": 8,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "user tap on the Animation section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "tap on the view Flip option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "as user click on the Flip button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "whole list of options get flip to another language",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 516100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.user_tap_on_the_Animation_section()"
+});
+formatter.result({
+  "duration": 1939430700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "FlipListSteps.tap_on_the_view_Flip_option()"
+});
+formatter.result({
+  "duration": 1293034400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "FlipListSteps.as_user_click_on_the_Flip_button()"
+});
+formatter.result({
+  "duration": 185139900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "FlipListSteps.whole_list_of_options_get_flip_to_another_language()"
+});
+formatter.result({
+  "duration": 176300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 300693300,
+  "status": "passed"
+});
+formatter.uri("Hide\u0026ShowButtons.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Featurefile for Animation Related Test",
+  "description": "       To verify the hiding and then showing of that buttons functionality under the animation section of DemoApi app.",
+  "id": "featurefile-for-animation-related-test",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 10970383200,
+  "status": "passed"
+});
+formatter.background({
+  "line": 5,
+  "name": "user is inside the hide-show animation section",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 7,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "user tap on the Animation section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "tap on the Hide-Show Animation section",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 458400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.user_tap_on_the_Animation_section()"
+});
+formatter.result({
+  "duration": 1832237800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.tap_on_the_Hide_Show_Animation_section()"
+});
+formatter.result({
+  "duration": 132408500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "To verify the working of hiding of buttons",
+  "description": "",
+  "id": "featurefile-for-animation-related-test;to-verify-the-working-of-hiding-of-buttons",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 12,
+      "name": "@test1"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "user is inside hideshow animation section of the app",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "user markcheck to provide some custom animation",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "start tapping the buttons to hide them",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "all buttons have gone invisible",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.user_is_inside_hideshow_animation_section_of_the_app()"
+});
+formatter.result({
+  "duration": 216000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.markcheck_to_provide_some_custom_animation()"
+});
+formatter.result({
+  "duration": 1443883700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.start_tapping_the_buttons_to_hide_them()"
+});
+formatter.result({
+  "duration": 794199200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.all_buttons_have_gone_invisible()"
+});
+formatter.result({
+  "duration": 448500,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 352511600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 10748897900,
+  "status": "passed"
+});
+formatter.background({
+  "line": 5,
+  "name": "user is inside the hide-show animation section",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 7,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "user tap on the Animation section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "tap on the Hide-Show Animation section",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 527000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.user_tap_on_the_Animation_section()"
+});
+formatter.result({
+  "duration": 2067308200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.tap_on_the_Hide_Show_Animation_section()"
+});
+formatter.result({
+  "duration": 158701600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "To verify the working of Show button to show hidden buttons",
+  "description": "",
+  "id": "featurefile-for-animation-related-test;to-verify-the-working-of-show-button-to-show-hidden-buttons",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 22,
+      "name": "@test2"
+    }
+  ]
+});
+formatter.step({
+  "line": 25,
+  "name": "user is inside hideshow animation section of the app",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "user markcheck to provide some custom animation",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 27,
+  "name": "start tapping the buttons to hide them",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "as user tap on the Show button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "all hidden buttons starts being visible",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.user_is_inside_hideshow_animation_section_of_the_app()"
+});
+formatter.result({
+  "duration": 163100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.markcheck_to_provide_some_custom_animation()"
+});
+formatter.result({
+  "duration": 1678656500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.start_tapping_the_buttons_to_hide_them()"
+});
+formatter.result({
+  "duration": 810450500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.as_user_tap_on_the_Show_button()"
+});
+formatter.result({
+  "duration": 1131546900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AnimationFuncSteps.all_hidden_buttons_starts_being_visible()"
+});
+formatter.result({
+  "duration": 1347600,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 279997500,
+  "status": "passed"
+});
+formatter.uri("SMSMessaging.feature");
+formatter.feature({
+  "line": 1,
+  "name": "To verify the SMS Messaging functionality in DemoApi app.",
+  "description": "",
+  "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 5,
+  "name": "Send an SMS",
+  "description": "",
+  "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.;send-an-sms",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@test4"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "user tap on the OS section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "select the SMS messaging option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "write \"\u003cmobile_no\u003e\" within recipient section",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "write \"\u003cMsg_content\u003e\" within message body",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "click on Send button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "this \"\u003cverification_msg\u003e\" of successfull sent appear on screen",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 16,
+  "name": "",
+  "description": "",
+  "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.;send-an-sms;",
+  "rows": [
+    {
+      "cells": [
+        "mobile_no",
+        "Msg_content",
+        "verification_msg"
+      ],
+      "line": 17,
+      "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.;send-an-sms;;1"
+    },
+    {
+      "cells": [
+        "9565715861",
+        "Hello Peter!",
+        "Message sent!"
+      ],
+      "line": 18,
+      "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.;send-an-sms;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 12313457800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Send an SMS",
+  "description": "",
+  "id": "to-verify-the-sms-messaging-functionality-in-demoapi-app.;send-an-sms;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@test4"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "user tap on the OS section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "select the SMS messaging option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "write \"9565715861\" within recipient section",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "write \"Hello Peter!\" within message body",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "click on Send button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "this \"Message sent!\" of successfull sent appear on screen",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 629000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SMSMessagingSteps.user_tap_on_the_OS_section()"
+});
+formatter.result({
+  "duration": 552199800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SMSMessagingSteps.select_the_SMS_messaging_option()"
+});
+formatter.result({
+  "duration": 2749687100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "9565715861",
+      "offset": 7
+    }
+  ],
+  "location": "SMSMessagingSteps.write_within_recipient_section(String)"
+});
+formatter.result({
+  "duration": 250965100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Hello Peter!",
+      "offset": 7
+    }
+  ],
+  "location": "SMSMessagingSteps.write_within_message_body(String)"
+});
+formatter.result({
+  "duration": 703801300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SMSMessagingSteps.click_on_Send_button()"
+});
+formatter.result({
+  "duration": 574478600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Message sent!",
+      "offset": 6
+    }
+  ],
+  "location": "SMSMessagingSteps.this_of_successfull_sent_appear_on_screen(String)"
+});
+formatter.result({
+  "duration": 248100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 399084400,
+  "status": "passed"
+});
+formatter.uri("SwipeOverGallaryImages.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Swipe_to_view_Images",
+  "description": "     To verify the horizontal swipe functionality over the gallary images present in app.",
+  "id": "swipe-to-view-images",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 13378906500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 7,
+  "name": "View All images of gallery through swiping horizontally",
+  "description": "",
+  "id": "swipe-to-view-images;view-all-images-of-gallery-through-swiping-horizontally",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@test5"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "application has already opened",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 10,
+  "name": "user tap on the view section",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "go inside the gallary section",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "select the Photos option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "images of gallary gets visible",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "User swipes horizontally from right to left",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 15,
+  "name": "more images of nature get seen",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AnimationFuncSteps.application_has_already_opened()"
+});
+formatter.result({
+  "duration": 545800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.user_tap_on_the_view_section()"
+});
+formatter.result({
+  "duration": 1926412600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.go_inside_the_gallary_section()"
+});
+formatter.result({
+  "duration": 1368273700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.select_the_Photos_option()"
+});
+formatter.result({
+  "duration": 1606435500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.images_of_gallary_gets_visible()"
+});
+formatter.result({
+  "duration": 465800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.user_swipes_horizontally_from_right_to_left()"
+});
+formatter.result({
+  "duration": 2955594600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SwipeOverImagesSteps.more_images_of_nature_get_seen()"
+});
+formatter.result({
+  "duration": 138800,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 266264500,
+  "status": "passed"
+});
+});
